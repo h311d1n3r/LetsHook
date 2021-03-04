98a1fef5280994e8f0e2907ae629d039fabb6ae6
HellDiner <me.helldiner@gmail.com>
2021-03-04 20:53:36 +0100

    cleaner code

--- src/hook_injector.cpp
+++ src/hook_injector.cpp
@@ -84,66 +84,65 @@ SIZE_T HookInjector::injectHookCall() {
 	char* hookFuncAddr = (char*)&(this->hook.hookFunc);
 	int codeLen = this->hook.codeLen;
 
-	string addRsp = Assembler::addChar(Register::RSP, 0x28);
+	string hookCallCode = Assembler::addChar(Register::RSP, 0x28);
+
 	Register main_reg_push_routine[]{
 	Register::R9,
 	Register::R8,
 	Register::RDX,
 	Register::RCX
 	};
-	string main_reg_pushs = Assembler::pushs(main_reg_push_routine, 4);
 
-	string fixRsp = Assembler::subChar(Register::RSP, 0x8);
+	hookCallCode.append(Assembler::pushs(main_reg_push_routine, 4));
+	hookCallCode.append(Assembler::subChar(Register::RSP, 0x8));
+	hookCallCode.append(Assembler::subInt(Register::RSP, 0x100)); //secure function parameters
 
-	string subRsp = Assembler::subInt(Register::RSP, 0x100); //secure function parameters
-
-	Register push_routine[]{
+	Register func_reg_push_routine[]{
 	Register::RAX,
 	Register::RBX,
 	Register::R10,
 	Register::R11,
 	Register::RBP
 	};
-	string pushs = Assembler::pushs(push_routine, 5);
 
-	string movRsp = Assembler::mov(Register::RCX, Register::RSP);
-
-	string addRcx = Assembler::addInt(Register::RCX, 0x130); //rcx now holds stack starting at rcx home
-
-	string subRsp2 = Assembler::subChar(Register::RSP, 0x50); //secure push routine
+	hookCallCode.append(Assembler::pushs(func_reg_push_routine, 5));
+	hookCallCode.append(Assembler::mov(Register::RCX, Register::RSP));
+	hookCallCode.append(Assembler::addInt(Register::RCX, 0x130)); //rcx now holds stack starting at rcx home
+	hookCallCode.append(Assembler::subChar(Register::RSP, 0x50)); //secure push routine
 
 	string call = {
 		0x48,(char)0xbd,hookFuncAddr[0],hookFuncAddr[1],hookFuncAddr[2],hookFuncAddr[3],hookFuncAddr[4],hookFuncAddr[5],hookFuncAddr[6],hookFuncAddr[7], //mov rbp, 0xXXXXXXXXXXXXXXXX
 		(char)0xff, (char)0xd5, // call rbp
 	};
 
-	string addRsp2 = Assembler::addChar(Register::RSP, 0x50);
+	hookCallCode.append(call);
+	hookCallCode.append(Assembler::addChar(Register::RSP, 0x50));
 
-	Register pop_routine[]{
+	Register func_reg_pop_routine[]{
 	Register::RBP,
 	Register::R11,
 	Register::R10,
 	Register::RBX,
 	Register::RAX
 	};
-	string pops = Assembler::pops(pop_routine, 5);
 
-	string addRsp3 = Assembler::addInt(Register::RSP, 0x108);
-
-	string movRcx = Assembler::movSrcPtr(Register::RCX, Register::RSP);
-	string addRsp4 = Assembler::addChar(Register::RSP, 0x8);
-	string movRdx = Assembler::movSrcPtr(Register::RDX, Register::RSP);
-	//addRsp4
-	string movR8 = Assembler::movSrcPtr(Register::R8, Register::RSP);
-	//addRsp4
-	string movR9 = Assembler::movSrcPtr(Register::R9, Register::RSP);
-
-	string subRsp3 = Assembler::subChar(Register::RSP, 0x20);
+	hookCallCode.append(Assembler::pops(func_reg_pop_routine, 5));
+	hookCallCode.append(Assembler::addInt(Register::RSP, 0x108));
+	hookCallCode.append(Assembler::movSrcPtr(Register::RCX, Register::RSP));
+	hookCallCode.append(Assembler::addChar(Register::RSP, 0x8));
+	hookCallCode.append(Assembler::movSrcPtr(Register::RDX, Register::RSP));
+	hookCallCode.append(Assembler::addChar(Register::RSP, 0x8));
+	hookCallCode.append(Assembler::movSrcPtr(Register::R8, Register::RSP));
+	hookCallCode.append(Assembler::addChar(Register::RSP, 0x8));
+	hookCallCode.append(Assembler::movSrcPtr(Register::R9, Register::RSP));
+	hookCallCode.append(Assembler::subChar(Register::RSP, 0x20));
 
 	string assemblyCopy = "";
 	for (int i = 0; i < codeLen; i++) {
 		assemblyCopy.push_back(*reinterpret_cast<char*>(this->hook.addr + i));
 	}
+
+	hookCallCode.append(assemblyCopy);
 
 	SIZE_T jmpBackAddr = this->hook.addr + 0xe; //0xe -> jmp length
 	char* jmpBackArr = (char*)&jmpBackAddr;
@@ -152,27 +151,6 @@ SIZE_T HookInjector::injectHookCall() {
 		jmpBackArr[0],jmpBackArr[1],jmpBackArr[2],jmpBackArr[3],jmpBackArr[4],jmpBackArr[5],jmpBackArr[6],jmpBackArr[7] //jmp 0xXXXXXXXXXXXXXXXX
 	};
 
-	string hookCallCode = addRsp;
-	hookCallCode.append(main_reg_pushs);
-	hookCallCode.append(fixRsp);
-	hookCallCode.append(subRsp);
-	hookCallCode.append(pushs);
-	hookCallCode.append(movRsp);
-	hookCallCode.append(addRcx);
-	hookCallCode.append(subRsp2);
-	hookCallCode.append(call);
-	hookCallCode.append(addRsp2);
-	hookCallCode.append(pops);
-	hookCallCode.append(addRsp3);
-	hookCallCode.append(movRcx);
-	hookCallCode.append(addRsp4);
-	hookCallCode.append(movRdx);
-	hookCallCode.append(addRsp4);
-	hookCallCode.append(movR8);
-	hookCallCode.append(addRsp4);
-	hookCallCode.append(movR9);
-	hookCallCode.append(subRsp3);
-	hookCallCode.append(assemblyCopy);
 	hookCallCode.append(jmpBack);
 
 	DWORD_PTR allocBase = this->allocateMemory(hookCallCode.size());
